c3e9b4ae8c067d7e19dd9030a677c5a5a1c28b40
Andy <andy@Andys-MacBook-Pro.local>
2019-05-29 20:30:43 +0200

    added api to lazy page

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -1,23 +1,62 @@
-import {NgModule, Component} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
+
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+import { CosmicService } from '../services/cosmic.service'
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-lazy-view',
   template: `
-    <h3>This is content from a lazy-loaded route</h3>
+
+  
+  
+    <h3 *ngFor="let blog of allBlogs">{{blog.title}}</h3>
     <div>Check your Networks tab to see that the js file got loaded</div>
     <br>
     <div><em>/lazy/nested/</em> routes to the same page</div>
+    
   `
 })
-export class LazyComponent {}
+export class LazyComponent implements OnInit {
+
+  data;
+  allBlogs;
+  allPosts;
+  author;
+  jdata;
+
+  constructor(private _http: Http, private route: Router, private cosmicService: CosmicService) {  this.showAllBlogs()}
+
+//fetching all blogs from server
+showAllBlogs() {
+  this.cosmicService.showAllBlogs()
+ .subscribe(res => {
+      this.data = res;
+      var jsondata = JSON.parse(this.data._body);
+      this.allBlogs = jsondata.objects;
+      console.log(this.allBlogs);
+    })
+}
+
+ngOnInit() {
+
+  this.showAllBlogs()
+}
+
+}
+
+
 
 @NgModule({
   declarations: [LazyComponent],
   imports: [
     RouterModule.forChild([
       { path: '', component: LazyComponent, pathMatch: 'full'}
-    ])
+    ]),
+    CommonModule
   ]
 })
 export class LazyModule {
