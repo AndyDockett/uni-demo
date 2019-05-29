edb1c1ee817b3feeca697cca7e7a1afb798c91c1
Andy <andy@Andys-MacBook-Pro.local>
2019-05-29 15:09:12 +0200

    addedcosmic

---
+++ src/config/cosmo.config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    production: false,
+    read_key: 'u54tfLMMYBd8jWpfrZE3z0p7pb7hPYrONn36Q9dqlv4zTEDDhx',
+    write_key: 'gneFYsCTFXen5Jnp34OZ0tl7C6tyXJWikxgYGCfnV3aNhYyZ3J',
+    bucket_id: '5ced99c009a4ce4d8646c61c',
+    bucket_slug: 'cosmic-proto',
+    URL: 'https://api.cosmicjs.com/v1/',
+    };
+    
