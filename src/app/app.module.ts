4a7c602df5a7d29b161f26512b21989f7a2c2ac4
Andy <andy@Andys-MacBook-Pro.local>
2019-05-29 15:35:11 +0200

    out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
-import { RegisterComponent } from './components/register/register.component';
 
-import { UsersinglepostComponent } from './components/usersinglepost/usersinglepost.component';
 
 
 
