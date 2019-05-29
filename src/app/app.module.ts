ceafcb9bdc16c9b06c64afc8e649c5b59a99734a
Andy <andy@Andys-MacBook-Pro.local>
2019-05-29 15:21:28 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { AuthGuard } from './services/auth-guard.service';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
 import { AllblogsComponent } from './components/allblogs/allblogs.component';
-import { LoginComponent } from './components/login/login.component';
+
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
@@ -9,30 +9,28 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
-import { RegisterComponent } from './components/register/register.component';
-import { SinglepostComponent } from './components/singlepost/singlepost.component';
-import { UsersinglepostComponent } from './components/usersinglepost/usersinglepost.component';
 
+import { SinglepostComponent } from './components/singlepost/singlepost.component';
+
+import 'rxjs/add/operator/map'
 import { NgxEditorModule } from 'ngx-editor';
 
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
-import { ModalModule } from 'ngx-bootstrap/modal';
-import 'rxjs/add/operator/map'
+
 import { CosmicService} from './services/cosmic.service'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    RegisterComponent,
+   
     AllblogsComponent,
-    DashboardComponent,
+   
   
     SinglepostComponent,
-    UsersinglepostComponent
+   
 
   ],
   imports: [
@@ -44,32 +42,19 @@ import { CosmicService} from './services/cosmic.service'
 
 
       
-        {
-            path: 'login',
-            component: LoginComponent
-          },
-          {
-            path: 'register',
-            component: RegisterComponent
-          },
+       
           {
             path: 'monkey',
             component: AllblogsComponent
           },
           
-          { path: 'dashboard', component: DashboardComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+         
+          
           {
             path: 'singlepost',
             component: SinglepostComponent
           },
-          {
-            path: 'singlepost',
-            component: SinglepostComponent
-          },
-          {
-            path: 'dashboard/usersinglepost',
-            component: UsersinglepostComponent
-          }
+          
     
     
 
@@ -83,10 +68,10 @@ import { CosmicService} from './services/cosmic.service'
     NgxEditorModule,
     HttpClientModule,
     HttpModule,
-    ModalModule.forRoot(),
+   
    
   ],
-  providers: [HttpClient, AuthGuard, CosmicService],
+  providers: [HttpClient, CosmicService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
